88da5ce8d8a60f5e311dcb1f04b25fa0b5698c32
Hayrettin ULUDERE <hayrettin.uludere@manisasu.gov.tr>
2024-12-09 15:51:06 +0300

    ilk react proje kodları

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/logo192.png" />
+      <img src={logo} /> 
+    </div>
+  );
+}
+
+export default App;
